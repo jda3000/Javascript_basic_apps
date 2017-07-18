987f04341a3fa7d333dc959298e8cc124cd54d6f
jda3000 <mrrewire@gmail.com>
2017-07-18 14:11:12 +0100

    Moving Buttons
    Basic javascript

---
+++ Moving Buttons/app.js
@@ -0,0 +1,25 @@
+//grab everything we need
+
+const buttons = document.querySelectorAll('.btn-crazy');
+console.log(buttons);
+
+//functions
+
+function app() {
+    console.log('hello');
+    
+    //get randon numbers
+    
+    let offsetLeft = (Math.random() * (window.innerWidth - this.clientWidth));
+    let offsetTop = (Math.random() * (window.innerHeight - this.clientHeight));
+    
+    //apply random numbers
+    
+    this.style.left = offsetLeft + 'px';
+    this.style.top = offsetTop + 'px';
+    
+}
+
+//add event listeners
+
+buttons.forEach(button => button.addEventListener('mouseenter', app));
