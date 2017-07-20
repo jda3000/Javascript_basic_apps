6e4ae377b7e2709fc3fd05e311e6d708424f5326
jda3000 <mrrewire@gmail.com>
2017-07-20 18:01:26 +0100

    Accordion

---
+++ Accordion/app.js
@@ -0,0 +1,26 @@
+// get everything needed
+
+const accordion = document.querySelector('.accordion');
+
+const items = accordion.querySelectorAll('li');
+const question = accordion.querySelectorAll('.question');
+
+
+//functions
+
+function toggleaccordion() {
+    console.log('clicked');
+    
+    let itemClick = this.parentNode;
+    
+    items.forEach(item => {
+            if(itemClick == item) {
+            itemClick.classList.toggle('open');
+            return;     
+       }
+       item.classList.remove('open');
+   });
+}
+//event listeners
+
+question.forEach(question => question.addEventListener('click', toggleaccordion));
